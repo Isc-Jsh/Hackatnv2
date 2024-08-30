e8771c4b19a093b7877beb762d5c9362c2fd9dd9
isc_jsh <74984147+Isc-Jsh@users.noreply.github.com>
2024-08-30 03:44:13 +0000

    ok sin horas

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -4,13 +4,13 @@ import React from "react";
         // add logic
 
         const shelterHours = [
-            { day: "Monday", open: "10:00", close: "16:00" },
-            { day: "Tuesday", open: "10:00", close: "16:00" },
-            { day: "Wednesday", open: "10:00", close: "16:00" },
-            { day: "Thursday", open: "10:00", close: "16:00" },
-            { day: "Friday", open: "10:00", close: "16:00" },
-            { day: "Saturday", open: "9:00", close: "20:00" },
-            { day: "Sunday", open: "9:00", close: "20:00" },
+            { day: "Monday", open: "10:00", close: "23:50:00" },
+            { day: "Tuesday", open: "10:00", close: "23:50:00" },
+            { day: "Wednesday", open: "10:00", close: "23:00" },
+            { day: "Thursday", open: "10:00", close: "23:50:00" },
+            { day: "Friday", open: "10:00", close: "23:00" },
+            { day: "Saturday", open: "9:00", close: "23:00" },
+            { day: "Sunday", open: "9:00", close: "23:00" },
     
         ]
 
@@ -18,13 +18,13 @@ import React from "react";
         const today = new Date().toLocaleDateString('en-US', { weekday: 'long' });
 
         // get today's hours
-        const todayHours = shelterHours.find((day) => day.day === today);
+        // const todayHours = shelterHours.find((day) => day.day === today);
 
         // display today and the hours in an div with an id of hours
         return (
             <div id="hours">
                 <h2>Today's Hours</h2>
-                <p>{todayHours.day} {todayHours.open} - {todayHours.close}</p>
+               
             </div>
         )
     }
